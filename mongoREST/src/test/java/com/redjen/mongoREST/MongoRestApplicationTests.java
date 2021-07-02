21fabb86d478d96f5ba683071249b528300e9169
seokwoo.jeong <seokwoo.jeong@softcamp.co.kr>
2021-07-02 14:13:20 +0900

    mongoDB and RESTful API with Spring Boot

---
+++ mongoREST/src/test/java/com/redjen/mongoREST/MongoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redjen.mongoREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
