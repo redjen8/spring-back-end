51390e9f7a58f0105d0045aff753fcd0b8873844
redjen8 <seokwoo2525@gmail.com>
2021-07-26 21:37:05 +0900

    postsApiController added, missing files fixed

---
+++ 20210722/src/main/java/com/redjen/softcamp/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.redjen.softcamp.service.posts;
+
+import com.redjen.softcamp.domain.posts.PostsRepository;
+import com.redjen.softcamp.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
